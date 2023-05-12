0c0a24161e00fb2e2c09e76606866795cb271521
darkknight1599 <59275566+darkknight1599@users.noreply.github.com>
2023-05-12 11:24:21 +0530

    build

---
+++ build/precache-manifest.44b75338338b80876b06e1833819c2e9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "80edda950d8b42e7eb2a",
+    "url": "/demo/react/abstrak/static/css/main.f344866e.chunk.css"
+  },
+  {
+    "revision": "80edda950d8b42e7eb2a",
+    "url": "/demo/react/abstrak/static/js/main.0b73c36f.chunk.js"
+  },
+  {
+    "revision": "4f0a719c95fca274090a",
+    "url": "/demo/react/abstrak/static/js/runtime~main.ddfa192d.js"
+  },
+  {
+    "revision": "f901aede5eb5510ada03",
+    "url": "/demo/react/abstrak/static/css/2.32c07460.chunk.css"
+  },
+  {
+    "revision": "f901aede5eb5510ada03",
+    "url": "/demo/react/abstrak/static/js/2.a97c8741.chunk.js"
+  },
+  {
+    "revision": "3a3f29b0c25992f59804",
+    "url": "/demo/react/abstrak/static/js/3.9db6ac3e.chunk.js"
+  },
+  {
+    "revision": "3ccc2376d9217ceef43dea2fd9614dd5",
+    "url": "/demo/react/abstrak/static/media/asparrowtech-logo.3ccc2376.png"
+  },
+  {
+    "revision": "f2c56a5a13f4ba5263365910d3848204",
+    "url": "/demo/react/abstrak/static/media/top-website-development-companies.f2c56a5a.svg"
+  },
+  {
+    "revision": "29e369785386efdc337be32e6781ee72",
+    "url": "/demo/react/abstrak/static/media/asparrow-bg.29e36978.jpeg"
+  },
+  {
+    "revision": "33d1a58a876bd0c92413c2184d723806",
+    "url": "/demo/react/abstrak/index.html"
+  }
+];
